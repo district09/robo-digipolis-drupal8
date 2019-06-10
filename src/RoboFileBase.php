8a9964e54cb0909023f3cbb4afa9f1a9ff9083b6
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2019-06-10 11:41:38 +0200

    Only use the first alias if multiple point to the same directory.
    This prevents errors with duplicate backup names.

--- src/RoboFileBase.php
+++ src/RoboFileBase.php
@@ -1097,6 +1097,24 @@ class RoboFileBase extends AbstractRoboFile
            return $aliases;
         }
         include $sitesFile;
-        return isset($sites) && is_array($sites) ? ($aliases + $sites) : $aliases;
+        $aliases = isset($sites) && is_array($sites) ? ($aliases + $sites) : $aliases;
+        /**
+         * Multiple aliases can map to the same folder. We don't want to execute
+         * every action for the same folder twice. For consistency, we use the
+         * url of the first occurrence of the folder name. Since array_unique()
+         * sorts the array before filtering it, we can't use that. Using the
+         * array_flip() function, a value has several occurrences, the latest
+         * key will be used as its value, and all others will be lost. So to get
+         * the first occurence, we reverse the array, flip it twice, and reverse
+         * it again, so we have a unique array in the order we expect it to be.
+         */
+        return array_reverse(
+            array_flip(
+                array_flip(
+                    array_reverse($aliases, true)
+                )
+            ),
+            true
+        );
     }
 }
